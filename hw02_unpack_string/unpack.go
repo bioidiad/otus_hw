9353f4538c49487771eb4b769f67aecfe4361cb2
Aleksndr Ivanov <aleksandr.ivanov@edgecenter.ru>
2023-01-06 19:17:48 +0400

    hw_2 prepared

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,14 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(sentence string) (string, error) {
-	for _, char := range sentence {
-		fmt.Println(char)
+func Reverse(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
 	}
-	return "", nil
+	return string(rns)
+}
+
+func Unpack(s string) (string, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(rune(s[0])) {
+		return "", ErrInvalidString
+	}
+
+	var (
+		err error
+		b   strings.Builder
+	)
+
+	s = Reverse(s)
+	tmp := rune(s[0])
+	counter := 1
+
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			if unicode.IsDigit(tmp) {
+				return "", ErrInvalidString
+			}
+			counter, err = strconv.Atoi(string(r))
+			if err != nil {
+				return "", err
+			}
+		} else {
+			b.WriteString(strings.Repeat(string(r), counter))
+			counter = 1
+		}
+		tmp = r
+	}
+
+	return Reverse(b.String()), nil
 }
